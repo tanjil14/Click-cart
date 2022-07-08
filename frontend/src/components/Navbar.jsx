e534547622b4db264c93956baea67d62e428eed4
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-08 10:12:18 +0600

    adding nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 import styled from "styled-components";
 const Container = styled.div`
   height: 60px;
@@ -17,11 +19,23 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
-  
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
 `;
 const Center = styled.div`
   flex: 1;
   text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const Right = styled.div`
   flex: 1;
@@ -29,15 +43,34 @@ const Right = styled.div`
   align-items: center;
   justify-content: flex-end;
 `;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
         <Left>
           <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search..." />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>TANJIL</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
