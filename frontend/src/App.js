f061e201c3313d6466e5b9255dd6d68cba028b5b
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-09 16:59:19 +0600

    adding register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
       {/* <Home /> */}
       {/* <Product/> */}
-      <Login/>
+      {/* <Login/> */}
+      <Register/>
     </div>
   );
 }
---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+const Container=styled.div``
+const Cart = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+export default Cart;
