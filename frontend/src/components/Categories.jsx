a89c865e2604c31c243d5234babae1b26d24573f
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-10 15:46:14 +0600

    resonsive newsletter

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -6,7 +6,7 @@ const Container = styled.div`
   display: flex;
   padding: 20px;
   justify-content: space-between;
-  ${mobile({padding:"0px",flexDirection:"column"})}
+  ${mobile({padding:"5px",flexDirection:"column"})}
 `;
 
 const Categories = () => {
--- frontend/src/components/Newsletter.jsx
+++ frontend/src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Send } from "@mui/icons-material";
+import { mobile } from "../responsive";
 const Container = styled.div`
   height: 60vh;
   background-color: #fcf5f5;
@@ -16,6 +17,7 @@ const Desc = styled.div`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
 
 const InputContainer = styled.div`
@@ -25,6 +27,7 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "80%" })}
 `;
 const Input = styled.input`
   border: none;
