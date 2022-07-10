63b8300e912bd267de619a4c3915b39f9efdb728
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-10 15:41:27 +0600

    category item responsive

--- frontend/src/components/CategoryItem.jsx
+++ frontend/src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 const Container = styled.div`
   flex: 1;
   margin: 3px;
@@ -9,6 +10,7 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  ${mobile({height:"30vh"})}
 `;
 const Info = styled.div`
   position: absolute;
@@ -20,10 +22,12 @@ const Info = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  ${mobile({marginTop:"20px"})}
 `;
 const Title = styled.h1`
   color: white;
   margin-bottom: 20px;
+  ${mobile({fontSize:"28px"})}
 `;
 const Button = styled.button`
   border: none;
