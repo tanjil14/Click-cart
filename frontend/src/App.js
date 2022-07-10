14375bca8e1e88bc3f1f1da47b2674974fb604f3
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-10 16:03:07 +0600

    responsive list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,8 @@ function App() {
   return (
     <div>
       {/* <Home /> */}
-      <Product/>
+      {/* <Product/> */}
+      <ProductList/>
       {/* <Login/> */}
       {/* <Register/> */}
       {/* <Cart/> */}
