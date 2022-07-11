9f70ddd9f15eb80e1af2cd951ac931459d6bde65
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-11 10:25:59 +0600

    add product model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,50 +2,30 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: true,
-      trim: true,
-      min: 3,
-      max: 20,
-    },
-    lastName: {
-      type: String,
-      required: true,
-      trim: true,
-      min: 3,
-      max: 20,
-    },
     username: {
       type: String,
       required: true,
       trim: true,
       unique: true,
-      index: true,
-      lowercase: true,
     },
     email: {
       type: String,
       required: true,
       trim: true,
       unique: true,
-      lowercase: true,
     },
     password: {
       type: String,
       required: true,
     },
-    role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-    contactNumber: { type: String },
-    profilePicture: { type: String },
   },
   { timestamps: true }
 );
-UserSchema.virtual("fullName").get(function () {
-  return `${this.firstName} ${this.lastName}`;
-});
+// UserSchema.virtual("fullName").get(function () {
+//   return `${this.firstName} ${this.lastName}`;
+// });
 export default mongoose.model("User", UserSchema);
