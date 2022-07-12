1f122716b14402083cf592f69cbee53b697fd75e
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-12 15:00:05 +0600

    adding update

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -1,10 +1,25 @@
-import Product from "../models/Product.js"
-export const createProduct=async(req,res)=>{
-    const newProduct=new Product(req.body)
-    try {
-        const savedProduct=await newProduct.save();
-        res.status(200).json(savedProduct)
-    } catch (error) {
-        res.status(500).json(error);
-    }
-}
+import Product from "../models/Product.js";
+export const createProduct = async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+//UPDATE
+export const updateProduct = async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,7 +1,8 @@
 import express from "express"
-import { createProduct } from "../controllers/product.js"
+import { createProduct,updateProduct } from "../controllers/product.js"
 import { verifyTokenAndAdmin } from "../utils/verifyToken.js"
 const router=express.Router()
 
 router.post("/",verifyTokenAndAdmin,createProduct)
+router.put("/:id",verifyTokenAndAdmin,updateProduct)
 export default router;
