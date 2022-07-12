7892dcad3b9311fa1d6b11626f54a32f99ea736d
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-12 13:48:27 +0600

    all user api

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -30,12 +30,24 @@ export const deleteUser = async (req, res) => {
     res.status(500).json(error);
   }
 };
-
+//GET USER
 export const getById = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const { password, ...others } = user._doc;
     res.status(200).json(others);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+////GET ALL USER
+export const allUsers = async (req, res) => {
+  const query = req.query?.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json(error);
   }
