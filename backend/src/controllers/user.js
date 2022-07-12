3cc61ba7ca2654b00676c1ebc0ba94c60b602657
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-12 14:16:11 +0600

    adding stats

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -52,3 +52,31 @@ export const allUsers = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+//GET USER STATS
+
+export const stats = async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      {
+        $match: { createdAt: { $gte: lastYear } }
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { deleteUser, updateUser,getById} from "../controllers/user.js";
+import { deleteUser, updateUser,getById, allUsers, stats} from "../controllers/user.js";
 import { verifyTokenAndAdmin, verifyTokenAndAuthorization } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.put("/:id", verifyTokenAndAuthorization, updateUser);
 router.delete("/:id",verifyTokenAndAdmin,deleteUser)
 router.get("/find/:id",verifyTokenAndAdmin,getById)
+router.get("/",verifyTokenAndAdmin,allUsers)
+router.get("/stats",verifyTokenAndAdmin,stats)
 
 
 export default router;
