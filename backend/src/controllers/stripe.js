694fb9d272ab018f84781b09205f81a61edf3751
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-13 14:46:27 +0600

    create stripe api

--- backend/src/controllers/stripe.js
+++ backend/src/controllers/stripe.js
@@ -1,3 +1,18 @@
-export const createPayment=async(req,res)=>{
-    
-}
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_KEY);
+export const createPayment = async (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+};
