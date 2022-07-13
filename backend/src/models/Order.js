29c335be13ab8dca56976193eef1defe02397bf2
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-13 11:51:59 +0600

    order income

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -1,20 +1,23 @@
 import mongoose from "mongoose";
 
-const OrderSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  products: [
-    {
-      productId: {
-        type: String,
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
       },
-      quantity: {
-        type: Number,
-        default: 1,
-      },
-    },
-  ],
-  amount: { type: Number, required: true },
-  address: { type: Object, required: true },
-  status: { type: String, default: "pending" },
-});
-export default mongoose.model("Order",OrderSchema)
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("Order", OrderSchema);
--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -3,6 +3,7 @@ import {
   allOrders,
   createOrder,
   deleteOrder,
+  monthlyIncome,
   updateOrder,
   userOrder,
 } from "../controllers/order.js";
@@ -18,4 +19,5 @@ router.put("/:id", verifyTokenAndAdmin, updateOrder);
 router.delete("/:id", verifyTokenAndAdmin, deleteOrder);
 router.get("/find/:userId", verifyTokenAndAuthorization, userOrder);
 router.get("/", verifyTokenAndAdmin, allOrders);
+router.get("/income", verifyTokenAndAdmin, monthlyIncome);
 export default router;
