21fbe05413825991634a314c65e79090f4f1d096
tanjil114 <tanjil14@cse.pstu.ac.bd>
2022-07-13 11:17:01 +0600

    user order api

--- backend/src/controllers/order.js
+++ backend/src/controllers/order.js
@@ -33,3 +33,12 @@ export const deleteOrder = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+export const userOrder = async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- backend/src/routes/order.js
+++ backend/src/routes/order.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createOrder, deleteOrder, updateOrder } from "../controllers/order.js";
-import { verifyToken, verifyTokenAndAdmin } from "../utils/verifyToken.js";
+import { createOrder, deleteOrder, updateOrder,userOrder } from "../controllers/order.js";
+import { verifyToken, verifyTokenAndAdmin, verifyTokenAndAuthorization } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.post("/", verifyToken, createOrder);
 router.put("/:id", verifyTokenAndAdmin, updateOrder);
 router.delete("/:id", verifyTokenAndAdmin, deleteOrder);
+router.get("/find/:userId", verifyTokenAndAuthorization, userOrder);
 export default router;
